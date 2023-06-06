c65f91e73a735ae6253bcd5dbc771a9063ab39b8
hesham156 <69813818+hesham156@users.noreply.github.com>
2023-06-06 22:17:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,14 +9,27 @@ const createElm = (type,elmClass,src)=>{
 }
 let link = createElm('link',false,`https://raw.githack.com/hesham156/salla/main/css/global.css`);
 document.head.appendChild(link)
-const choseThem=(themName)=>{
-    let link = createElm('link',false,`https://raw.githack.com/hesham156/salla/main/css/${themName}.css`);
-    document.head.appendChild(link)
-}
 const addCompanyName =(companyName,url)=>{
     let h5 = createElm('h5','text-center');
     h5.innerHTML=`صنع بحب بواسطه <a href=${url}>${companyName}</a>`;
     document.querySelector('footer').appendChild(h5);
 }
-// choseThem('raval')
-// addCompanyName('azzrk','hh')
+const choseThem=(themName,siteName,logo,BG,PColor,SColor,companyName,companyUrl)=>{
+    let style = document.createElement('style');
+    style.innerHTML=`
+        :root{
+            --primary-color:${PColor};
+            --secendery-color:${SColor};
+            --logo:url('${logo}');
+            --body-bg:url('${BG}');
+            --site-name:'الحقوق محفوظه لدي ${siteName}'
+        
+    }
+    `
+    let link = createElm('link',false,`https://raw.githack.com/hesham156/salla/main/css/${themName}.css`);
+    document.head.appendChild(link)
+    addCompanyName(companyName,companyUrl)
+
+}
+
+choseThem('raval')
